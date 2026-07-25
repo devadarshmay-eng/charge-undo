a7f5fff717ee0f2ff2fb5492d5402e2bc7e0f8c4
devadarsh <devadarsh.may@gmail.com>
2026-07-25 18:23:01 +0530

    Scope the no-scroll rule to the map app only

--- components/charge-map.tsx
+++ components/charge-map.tsx
@@ -154,6 +154,11 @@ export function ChargeMap({ initialLoc }: { initialLoc?: { lat: number; lng: num
         setReportOpen(true);
       }
     }
+
+    document.documentElement.classList.add("app-no-scroll");
+    return () => {
+      document.documentElement.classList.remove("app-no-scroll");
+    };
   }, []);
 
   useEffect(() => {
